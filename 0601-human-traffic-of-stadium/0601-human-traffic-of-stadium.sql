74daadba0f8706d0d6df1deef5a39295074c7e27
B Rukshith Naidu <144221895+Rukshit@users.noreply.github.com>
2026-08-24 10:18:34 +0530

    Time: 435 ms (34.02%), Space: 0B (100%) - LeetHub

---
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -0,0 +1,24 @@
+WITH Filtered AS (
+    SELECT 
+        id, 
+        visit_date, 
+        people,
+        id - ROW_NUMBER() OVER (ORDER BY id) AS grp
+    FROM Stadium
+    WHERE people >= 100
+),
+GroupCounts AS (
+    SELECT 
+        id, 
+        visit_date, 
+        people,
+        COUNT(*) OVER (PARTITION BY grp) AS grp_count
+    FROM Filtered
+)
+SELECT 
+    id, 
+    visit_date, 
+    people
+FROM GroupCounts
+WHERE grp_count >= 3
+ORDER BY visit_date ASC;
